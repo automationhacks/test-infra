a206994ba7c694b1af46800a363bd3d06bd99ee9
automationhacks <automationhacks@gmail.com>
2024-11-06 22:10:36 +0530

    - Skip running tests in the build phase

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,7 +22,7 @@ pipeline {
 
         stage('Build') {
             steps {
-                sh './gradlew clean build'
+                sh './gradlew clean build -x test'
             }
         }
 
